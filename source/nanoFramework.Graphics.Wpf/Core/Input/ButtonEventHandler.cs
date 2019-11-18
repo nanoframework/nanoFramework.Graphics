cd405238d116a8e27e8b7909993f299f66db8b90
Terry Fogg <Terry.Fogg@outlook.com>
2019-11-19 00:07:37 +1100

    Initial Code

---
+++ source/nanoFramework.Graphics.Wpf/Core/Input/ButtonEventHandler.cs
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+namespace nanoFramework.UI.Input
+{
+    /// <summary>
+    ///     The delegate to use for handlers that receive ButtonEventArgs.
+    /// </summary>
+    public delegate void ButtonEventHandler(object sender, ButtonEventArgs e);
+}
+
+
---
+++ source/nanoFramework.Graphics.Wpf/Core/Input/FocusChangedEventHandler.cs
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+namespace nanoFramework.UI.Input
+{
+    /// <summary>
+    ///     The delegate to use for handlers that receive FocusChangedEventArgs.
+    /// </summary>
+    public delegate void FocusChangedEventHandler(object sender, FocusChangedEventArgs e);
+}
+
+
