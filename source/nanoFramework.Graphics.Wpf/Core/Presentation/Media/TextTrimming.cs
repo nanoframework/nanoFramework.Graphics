8c0f7f99563ba3d242d77c2b2fd6ccd142bc86bf
TerryFogg <Terry.Fogg@outlook.com>
2019-11-21 00:03:52 +1100

    Code imported from .NETMF for use in nanoFramework (#1)

---
+++ source/nanoFramework.Graphics.Wpf/Core/Presentation/Media/TextTrimming.cs
@@ -0,0 +1,31 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+namespace nanoFramework.Presentation.Media
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum TextTrimming
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        CharacterEllipsis,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        WordEllipsis,
+    }
+}
+
+
---
+++ source/nanoFramework.Graphics.Wpf/Native/DisplayControl.cs
@@ -0,0 +1,106 @@
+﻿//
+// Copyright (c) 2019 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace nanoFramework.UI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum DisplayOrientation : int
+    {
+        /// <summary>
+        ///     Portrain
+        /// </summary>
+        PORTRAIT,
+        /// <summary>
+        ///     Portrait 180
+        /// </summary>
+        PORTRAIT180,
+        /// <summary>
+        ///     Landscape
+        /// </summary>
+        LANDSCAPE,
+        /// <summary>
+        ///     Landscape 180
+        /// </summary>
+        LANDSCAPE180
+    };
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DisplayControl : MarshalByRefObject, IDisposable
+    {
+        static DisplayControl()
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        extern public int LongerSide
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        extern public int ShorterSide
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        extern public int BitsPerPixel
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        extern public int Orientation
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Orientation"></param>
+        /// <returns></returns>
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern public bool ChangeOrientation(ref int Orientation);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="disposing"></param>
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern private void Dispose(bool disposing);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
+
