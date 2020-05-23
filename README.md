fe996fda219e91aae471bd5da9f4652b3684f2a0
TerryFogg <Terry.Fogg@outlook.com>
2020-05-24 05:44:41 +1000

    First Preview (#3)

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# lib-nanoFramework.Graphics.Wpf
- nanoFramework nanoFramework.Graphics.Wpf class library 
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=nanoframework_lib-nanoFramework.Graphics&metric=alert_status)](https://sonarcloud.io/dashboard?id=nanoframework_lib-nanoFramework.Graphics) [![Reliability Rating](https://sonarcloud.io/api/project_badges/measure?project=nanoframework_lib-nanoFramework.Graphics&metric=reliability_rating)](https://sonarcloud.io/dashboard?id=nanoframework_lib-nanoFramework.Graphics) [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://github.com/nanoframework/Home/blob/master/LICENSE) [![NuGet](https://img.shields.io/nuget/dt/nanoFramework.nanoFramework.Graphics.svg?label=NuGet&style=flat&logo=nuget)](https://www.nuget.org/packages/nanoFramework.Graphics/) [![#yourfirstpr](https://img.shields.io/badge/first--timers--only-friendly-blue.svg)](https://github.com/nanoframework/Home/blob/master/CONTRIBUTING.md)
+[![Discord](https://img.shields.io/discord/478725473862549535.svg?logo=discord&logoColor=white&label=Discord&color=7289DA)](https://discord.gg/gCyBu8T)
+
+![nanoFramework logo](https://github.com/nanoframework/Home/blob/master/resources/logo/nanoFramework-repo-logo.png)
+
+-----
+
+### Welcome to the **nanoFramework** Graphics repository!
+
+## Build status
+
+| Component | Build Status | NuGet Package |
+|:-|---|---|
+| nanoFramework.Graphics | [![Build Status](https://dev.azure.com/nanoframework/nanoFramework.Graphics/_apis/build/status/nanoframework.lib-nanoFramework.Graphics?branchName=master)](https://dev.azure.com/nanoframework/nanoFramework.Graphics/_build/latest?definitionId=57&branchName=master) | [![NuGet](https://img.shields.io/nuget/v/nanoFramework.Graphics.svg?label=NuGet&style=flat&logo=nuget)](https://www.nuget.org/packages/nanoFramework.Graphics/)  |
+| nanoFramework.Graphics (preview) | [![Build Status](https://dev.azure.com/nanoframework/nanoFramework.Graphics/_apis/build/status/nanoframework.lib-nanoFramework.Graphics?branchName=develop)](https://dev.azure.com/nanoframework/nanoFramework.Graphics/_build/latest?definitionId=57&branchName=develop) | [![](https://badgen.net/badge/NuGet/preview/D7B023?icon=https://simpleicons.now.sh/azuredevops/fff)](https://dev.azure.com/nanoframework/feed/_packaging?_a=package&feed=sandbox&package=nanoFramework.nanoFramework.Graphics&protocolType=NuGet&view=overview) |
+
+## Feedback and documentation
+
+For documentation, providing feedback, issues and finding out how to contribute please refer to the [Home repo](https://github.com/nanoframework/Home).
+
+Join our Discord community [here](https://discord.gg/gCyBu8T).
+
+## Credits
+
+The list of contributors to this project can be found at [CONTRIBUTORS](https://github.com/nanoframework/Home/blob/master/CONTRIBUTORS.md).
+
+## License
+
+The **nanoFramework** Class Libraries are licensed under the [Apache 2.0 license](http://www.apache.org/licenses/LICENSE-2.0).
+
+## Code of Conduct
+
+This project has adopted the code of conduct defined by the [Contributor Covenant](http://contributor-covenant.org/)
+to clarify expected behavior in our community.
---
+++ source/nanoFramework.Graphics/Primitive/DisplayControl.cs
@@ -0,0 +1,154 @@
+﻿//
+// Copyright (c) 2019 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace nanoFramework.UI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum DisplayOrientation : int
+    {
+        /// <summary>
+        ///     Portrain
+        /// </summary>
+        PORTRAIT,
+        /// <summary>
+        ///     Portrait 180
+        /// </summary>
+        PORTRAIT180,
+        /// <summary>
+        ///     Landscape
+        /// </summary>
+        LANDSCAPE,
+        /// <summary>
+        ///     Landscape 180
+        /// </summary>
+        LANDSCAPE180
+    };
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DisplayControl : MarshalByRefObject, IDisposable
+    {
+
+        static DisplayControl dc;
+         static DisplayControl()
+        {
+            dc = new DisplayControl();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static int ScreenWidth
+        {
+            get
+            {
+                //return dc.Width;
+                return 800;
+            }
+        }
+        // Gets the height of the screen
+        /// <summary>
+        /// 
+        /// </summary>
+        public static int ScreenHeight
+        {
+            get
+            {
+                //return dc.Height;
+                return 480;
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// The screens number of pixel for the longer side.
+        /// </summary>
+        extern public int LongerSide
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// The screens number of pixel for the shorter side.
+        /// </summary>
+        extern public int ShorterSide
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// The screens number of pixel for the width based on the orientation.
+        /// </summary>
+        extern public int Width
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// The screens number of pixel for the height based on the orientation.
+        /// </summary>
+        extern public int Height
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// Currently 16 bits in RBG565 format. ( There is some 1 bit code available but untested )
+        /// </summary>
+        extern public int BitsPerPixel
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// The orientation landscape, portrain
+        /// </summary>
+        extern public int Orientation
+        {
+            [MethodImplAttribute(MethodImplOptions.InternalCall)]
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Orientation"></param>
+        /// <returns></returns>
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern public bool ChangeOrientation(ref int Orientation);
+
+        /// <summary>
+        /// Release resources
+        /// </summary>
+        /// <param name="disposing"></param>
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern private void Dispose(bool disposing);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
+
