abe1316ee2c9182465fcb9896168b205fe0d297e
Laurent Ellerbach <laurelle@microsoft.com>
2021-09-09 09:39:08 +0200

    saqmple adjusted

--- GraphicTest/Program.cs
+++ GraphicTest/Program.cs
@@ -15,26 +15,30 @@ namespace GraphicTest
         public static void Main()
         {
             Thread.Sleep(2000);
+            const int BackLightPin = 32;
 
             Configuration.SetPinFunction(19, DeviceFunction.SPI1_MISO);
             Configuration.SetPinFunction(23, DeviceFunction.SPI1_MOSI);
             Configuration.SetPinFunction(18, DeviceFunction.SPI1_CLOCK);
-            Configuration.SetPinFunction(32, DeviceFunction.PWM1);
+            Configuration.SetPinFunction(BackLightPin, DeviceFunction.PWM1);
+            
 
-            //var pwmController = PwmController.FromId("TIM1");
-            //pwmController.SetDesiredFrequency(5000);
-            //var pwmPin = pwmController.OpenPin(32);
-            //pwmPin.SetActiveDutyCyclePercentage(0.1);
-            //pwmPin.Start();
-            GpioController controller = new();
-            GpioPin blPin = controller.OpenPin(32, PinMode.Output);
-            blPin.Write(PinValue.High);
+            //GpioController controller = new();
+            //GpioPin blPin = controller.OpenPin(BackLightPin, PinMode.Output);
+            //blPin.Write(PinValue.High);
 
             Debug.WriteLine("Hello from nanoFramework!");
 
-            DisplayControl.Initialize(new SpiConfiguration() { ChipSelect = 14, SpiBus = 1, DataCommand = 27, Reset = 33, BackLight = 32 }, 320, 240, 1024);
+            DisplayControl.Initialize(new SpiConfiguration() { ChipSelect = 14, SpiBus = 1, DataCommand = 27, Reset = 33, BackLight = BackLightPin }, 320, 240, 1024);
 
             Debug.WriteLine("Screen initialized");
+            var desc = PwmController.GetDeviceSelector();
+            Debug.WriteLine(desc);
+            PwmController pwm = PwmController.GetDefault();
+            pwm.SetDesiredFrequency(50000);
+            PwmPin pwmPin = pwm.OpenPin(BackLightPin);
+            pwmPin.SetActiveDutyCyclePercentage(0.1);
+            pwmPin.Start();
 
             int delayBetween = 1100;
 
