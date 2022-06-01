ae103f822447cbd059923ee030002d01a5710cfc
José Simões <jose.simoes@eclo.solutions>
2022-06-02 00:03:49 +0100

    Bump AssemblyNativeVersion to 100.0.0.4

--- nanoFramework.Graphics/Properties/AssemblyInfo.cs
+++ nanoFramework.Graphics/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
-[assembly: AssemblyNativeVersion("100.0.0.3")]
+[assembly: AssemblyNativeVersion("100.0.0.4")]
 ////////////////////////////////////////////////////////////////
 
 // Setting ComVisible to false makes the types in this assembly not visible 
