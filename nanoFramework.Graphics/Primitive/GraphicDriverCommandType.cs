f7a93997da1024ee4ae5a8575d5c5d0cb2db3386
Laurent Ellerbach <laurelle@microsoft.com>
2023-03-10 14:57:28 +0100

    Adding generic display driver (#118)
    * Adjusting for generic graphic drivers
    
    * adjusting
    
    * adjusting to remove warnings
    
    * adjusting pragma
    
    * fixing pragma
    
    * Add exclude rules for enums
    
    @Ellerbach check this out on how to exclude enums that are not used in native.
    With this they are not added to the stubs. 
    
    I'm not familiar with the code, so please check if any of these should indeed be there. And, of course, do add all the others that don't need to be there.
    
    ---------
    
    Co-authored-by: José Simões <jose.simoes@eclo.solutions>

---
+++ nanoFramework.Graphics/Primitive/GraphicDriverCommandType.cs
@@ -0,0 +1,24 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+namespace nanoFramework.UI
+{
+    /// <summary>
+    /// Graphic driver command types.
+    /// </summary>
+    public enum GraphicDriverCommandType
+    {
+        /// <summary>
+        /// Sleep command by chunk of 10 milliseconds.
+        /// </summary>
+        Sleep = 0,
+
+        /// <summary>
+        /// Normal command followed by data.
+        /// </summary>
+        Command = 1,
+    }
+}
---
+++ nanoFramework.Graphics/Primitive/SpiConfiguration.cs
@@ -0,0 +1,64 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+namespace nanoFramework.UI
+{
+    /// <summary>
+    /// The Spi configuration for the scressn
+    /// </summary>
+    public struct SpiConfiguration
+    {
+#pragma warning disable S4487 // nanoFramework doesn't support auto-properties
+        private byte _spiBus;
+        private int _chipSelect;
+        private int _dataCommand;
+        private int _reset;
+        private int _backLight;
+
+        /// <summary>
+        /// Creates a Spi configuration.
+        /// </summary>
+        /// <param name="spiBus">Spi bus.</param>
+        /// <param name="chipselect">Chip select.</param>
+        /// <param name="dataCommand">Data command.</param>
+        /// <param name="reset">Reset.</param>
+        /// <param name="backLight">Back light.</param>
+        public SpiConfiguration(byte spiBus, int chipselect, int dataCommand, int reset, int backLight)
+        {
+            _spiBus = spiBus;
+            _chipSelect = chipselect;
+            _dataCommand = dataCommand;
+            _reset = reset;
+            _backLight = backLight;
+        }
+
+        /// <summary>Z
+        /// Spi bus.
+        /// </summary>
+        public byte SpiBus { get => _spiBus; set => _spiBus = value; }
+
+        /// <summary>
+        /// Chip select.
+        /// </summary>
+        public int ChipSelect { get => _chipSelect; set => _chipSelect = value; }
+
+        /// <summary>
+        /// Data command.
+        /// </summary>
+        public int DataCommand { get => _dataCommand; set => _dataCommand = value; }
+
+        /// <summary>
+        /// Reset.
+        /// </summary>
+        public int Reset { get => _reset; set => _reset = value; }
+
+        /// <summary>
+        /// Back light
+        /// </summary>
+        public int BackLight { get => _backLight; set => _backLight = value; }
+#pragma warning restore S4487
+    }
+}
